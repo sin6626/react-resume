cd80ef86897ce4a9813aca8009e29838da5151c5
sin6626 <3554989283@qq.com>
2026-09-23 17:27:18 +0800

    chore: 网站图标设置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 导出问静态网站, 会导出在out文件夹内, 不加的会实在.next文件夹里, Next小细节, 使用svg图作为网站图标, 必须是icon.svg的名字
+  output: "export"
 };
 
 export default nextConfig;
